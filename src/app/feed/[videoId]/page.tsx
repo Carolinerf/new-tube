95e2caa60259e208367e2d7e7116dfd76f6ce0bf
Carolinerf <3412983269@qq.com>
2025-09-26 19:53:42 +0800

    Basic layout

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,17 @@
+interface PageProps {
+  params: Promise<{ videoId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  console.log("Server component");
+
+  const { videoId } = await params;
+
+  return (
+    <div>
+      Video Id: {videoId}
+    </div>
+  );
+};
+
+export default Page;
